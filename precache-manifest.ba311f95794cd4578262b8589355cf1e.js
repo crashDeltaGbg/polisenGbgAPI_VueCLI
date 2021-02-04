682c7b3646834a3774227ccf8fc7224693fa437b
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-02-04 21:27:43 +0100

    Commit site

---
+++ precache-manifest.ba311f95794cd4578262b8589355cf1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c8584aa6b85ec91482",
+    "url": "/css/about.2c61e0b8.css"
+  },
+  {
+    "revision": "958a5fb9ae0021ab5bdc",
+    "url": "/css/app.e574f31d.css"
+  },
+  {
+    "revision": "5a1667d569addaf70392e8123dffe9ec",
+    "url": "/img/polisbil_192.5a1667d5.png"
+  },
+  {
+    "revision": "c8430c16281b1a3cc54287b3bd741e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1c8584aa6b85ec91482",
+    "url": "/js/about.71259ab6.js"
+  },
+  {
+    "revision": "958a5fb9ae0021ab5bdc",
+    "url": "/js/app.5f90ebbc.js"
+  },
+  {
+    "revision": "6446c0e5541e27209363",
+    "url": "/js/chunk-vendors.a849759a.js"
+  },
+  {
+    "revision": "62245aa478f80f81aea87ef297356283",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
